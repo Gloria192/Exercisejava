267885cdf097334c02e493e34adabc011f8cc464
Gloria <mutuyegloria@gmail.com>
2025-03-24 23:21:06 -0700

    second committ

--- operators.java
+++ operators.java
@@ -16,11 +16,17 @@ public static void main(String[] args) {
     double a=15 ;
     double b=4;
     double quotient=a/b;
-    double c= 20 ;
-    double d= 6;
-    double remainder=c%d ;
-    System.out.println(remainder);
-    System.out.println(quotient);
-    System.out.println(product);
+    int c= 20 ;
+    int d= 6;
+    int remainder=c%d ;
+    double cost = 3 * 4.99;
+     int f=12 ;
+     int e=30;
+     int diff =e-f;
+     System.out.println("the differnt is ="+diff);
+    System.out.println("the cost is ="+cost);
+    System.out.println("remainder is ="+remainder);
+    System.out.println("the quotient is ="+quotient);
+    System.out.println("the product of two numuber is ="+product);
 }
 }
