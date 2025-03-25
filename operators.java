78c066d3b86eab2079a291c900689f30b457b88c
Gloria <mutuyegloria@gmail.com>
2025-03-24 23:10:23 -0700

    first committ

---
+++ operators.java
@@ -0,0 +1,26 @@
+// public class operators {
+
+//     public static void main(String[] args) {
+//         int i=10;
+//         int j=7;
+//         int sum=i+j;
+//         System.out.println(sum);
+//     }
+// }
+//multiplyications
+public class operators {
+public static void main(String[] args) {
+    int i=8 ;
+    int j=5;
+    int product=i*j;
+    double a=15 ;
+    double b=4;
+    double quotient=a/b;
+    double c= 20 ;
+    double d= 6;
+    double remainder=c%d ;
+    System.out.println(remainder);
+    System.out.println(quotient);
+    System.out.println(product);
+}
+}
