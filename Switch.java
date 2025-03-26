6bd866b457ca30a68bb8a02ccf9a00f4a7acf68f
Gloria <mutuyegloria@gmail.com>
2025-03-26 01:02:03 -0700

    for loop

---
+++ Switch.java
@@ -0,0 +1,36 @@
+// public class Switch {
+//     public static void main(String[] args) {
+//         switch(days)
+//     }
+// }
+// public class Switch{
+// public static void main(String[] args) {
+//     float a = 15; // Example value for 'a'. You can modify this as needed.
+    
+//     if (a < 0) {
+//         System.out.println("Freezing: " + a);
+//     } else if (a > 0 && a <=20) {
+//         System.out.println("Cold: " + a);
+//     } else {
+//         System.out.println("Warm: " + a);
+//     }
+// }
+
+    
+public class Switch {
+
+    public static void main(String[] args) {
+        int days ;
+        switch (days) {
+            case 1:
+                System.out.println("monday");
+                break;
+        case 2:
+        System.out.println("tuesday");
+            default:
+            System.out.println("no days found");
+            
+        }
+    }
+}
+
